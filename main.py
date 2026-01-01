bc78ece7a6668df92ee137dd1545e51325746fc7
ExiledMender <timo.anso@gmail.com>
2026-01-01 18:13:49 +0200

    Created asteroid.py, asteroidfield.py, Updated constants.py, main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_TURN_SPEED
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -12,17 +14,20 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     clock = pygame.time.Clock()
-    td = 0
+    dt = 0
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
 
-    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2) 
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    AsteroidField()
 
     while True:
-       
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
@@ -31,14 +36,13 @@ def main():
 
         screen.fill("black")
 
-        updatable.update(td)
+        updatable.update(dt)
 
         for obj in drawable:
             obj.draw(screen)
 
         pygame.display.flip()
-        
-        td = clock.tick(60) / 1000.0  # Limit to 60 FPS
+        dt = clock.tick(60) / 1000.0  # Limit to 60 FPS
 
 if __name__ == "__main__":
     main()
